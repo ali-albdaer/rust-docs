fae49922ee91af76c33f3c5e4458992b5987eb8d
Ali Firas <ali.albdaer.py@gmail.com>
2024-08-07 15:53:21 +0300

    update

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -3,12 +3,12 @@ use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    println!("Guess the number!");
+    println!("Guess The Number (1-100)");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
     loop {
-        println!("Please input your guess.");
+        println!("Enter a number:");
 
         let mut guess = String::new();
 
@@ -18,10 +18,11 @@ fn main() {
 
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
-            Err(_) => continue,
+            Err(_) => {
+                println!("Please enter a valid number");
+                continue;
+            }
         };
-
-        println!("You guessed: {guess}");
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
@@ -32,4 +33,6 @@ fn main() {
             }
         }
     }
+
+    println!("Program Finished");
 }
