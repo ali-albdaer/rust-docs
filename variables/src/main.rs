89f56c8728239f40524f3010a89b86c78a5029cb
Ali Firas <ali.albdaer.py@gmail.com>
2024-08-08 14:28:40 +0300

    mut

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,8 +1,16 @@
-const G: i32 = 7;
+const ZERO: &str = "0";
 
 fn main() {
-    let mut x = 5;
+    let x: i32 = 5;  // immutable variable can be shadowed by using let
     println!("The value of x is: {x}");
-    x = G;
+
+    let x = ZERO;  // shadowing (let needed to convert type)
     println!("The value of x is: {x}");
+
+    {  // inner scope
+        let x = 3;
+        println!("The value of x is: {x}");
+    }
+
+    println!("The value of x is: {x}");  // back to ZERO
 }
