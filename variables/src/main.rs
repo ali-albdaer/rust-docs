3107dcd749ca13c765c7d180bcda9536c1c68291
Ali Firas <ali.albdaer.py@gmail.com>
2024-08-08 21:27:19 +0300

    float 64

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -4,13 +4,21 @@ fn main() {
     let x: i32 = 5;  // immutable variable can be shadowed by using let
     println!("The value of x is: {x}");
 
-    let x = ZERO;  // shadowing (let needed to convert type)
+    let x: &str = ZERO;  // shadowing (let needed to convert type)
     println!("The value of x is: {x}");
 
     {  // inner scope
-        let x = 3;
+        let x: i32 = 3;
         println!("The value of x is: {x}");
     }
 
     println!("The value of x is: {x}");  // back to ZERO
+
+    let y: f64 = 5.0;
+
+    println!("The value of y is: {y}");
+
+    let y: f32 = 6.0;
+
+    println!("The value of y is: {y}");
 }
