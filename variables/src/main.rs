520fe3b55002f0956aabb4d3bb46756504864a49
Ali Firas <ali.albdaer.py@gmail.com>
2024-08-08 21:56:24 +0300

    e

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -21,4 +21,26 @@ fn main() {
     let y: f32 = 6.0;
 
     println!("The value of y is: {y}");
+
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+
+    let (a, b, c) = tup;
+
+    println!("The value of a is: {a}");
+    println!("The value of b is: {b}");
+    println!("The value of c is: {c}");
+
+    let five_hundred = tup.0;
+    let six_point_four = tup.1;
+    let one = tup.2;
+
+    println!("The value of five_hundred is: {five_hundred}");
+    println!("The value of six_point_four is: {six_point_four}");
+    println!("The value of one is: {one}");
+
+    let mut tup2: (i32, f64, i32) = (500, 6.4, 1);
+
+    tup2.2 = 2;
+
+
 }
