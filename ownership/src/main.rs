46c483f016dcfc57e0a71d14934bad200452d786
Ali Firas <ali.albdaer.py@gmail.com>
2024-08-10 13:36:52 +0300

    box

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,4 +1,10 @@
 fn main() {
+    let a = Box::new([0; 1_000_000]);  // Owner of the heap memory at frame 1.
+    let b = a;  // Transfer the ownership of the heap memory from a to b. Frame 2.
+
+    println!("{}", b[0]);  // Access the heap memory through b. Frame 3.
+    // println!("{}", a[0]);  // Error: value borrowed here after move.
+
     let first = String::from("Ferris"); //1
     let full = add_suffix(first); //4
     // println!("{first}"); Error: value borrowed here after move.
