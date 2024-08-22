3e6a3c5f7751b1c3a91d59726398bc68d3592aea
Ali Firas <ali.albdaer.py@gmail.com>
2024-08-22 15:32:25 +0300

    cargo fmt and comments

--- methods/src/main.rs
+++ methods/src/main.rs
@@ -1,7 +1,7 @@
 #[derive(Debug)]
 struct Rectangle {
     width: u32,
-    height: u32
+    height: u32,
 }
 
 impl Rectangle {
@@ -21,22 +21,22 @@ impl Rectangle {
     fn square(side_length: u32) -> Self {
         Self {
             width: side_length,
-            height: side_length
+            height: side_length,
         }
-    }  // Not a method!
+    } // Not a method!
 }
 
-fn main () {
+fn main() {
     let rect1: Rectangle = Rectangle {
         width: 30,
-        height: 50
+        height: 50,
     };
 
     let scale: u32 = 2;
 
     let rect2: Rectangle = Rectangle {
-        width: dbg!(10 * scale),  // Takes ownership but returns it.
-        height: 40
+        width: dbg!(10 * scale), // Takes ownership but returns it.
+        height: 40,
     };
 
     let one_in_two: bool = rect2.fits(&rect1);
@@ -45,8 +45,8 @@ fn main () {
     println!("One in two: {}\nTwo in one: {}", one_in_two, two_in_one);
     println!("Area of rect1: {}", rect1.area());
 
-    println!("Rect2: {:?}", rect2);  // :#? for pretty-print.
-    dbg!(&rect2);  // A reference is used because dbg! takes ownership.
+    println!("Rect2: {:?}", rect2); // :#? for pretty-print.
+    dbg!(&rect2); // A reference is used because dbg! takes ownership.
 
     let square1: Rectangle = Rectangle::square(20);
     println!("Square area: {}", square1.area());
@@ -60,11 +60,10 @@ fn main () {
 
     let mut rect3 = Rectangle {
         width: 25,
-        height: 35
+        height: 35,
     };
 
     println!("Initial Dimensions: {:#?}", rect3);
-    rect3.scale_by(3);
+    rect3.scale_by(3); // == Rectangle::scale_by(&rect3, 3)
     println!("After scaling: {:#?}", rect3);
-
 }
