18b8d9f4eaf2f3d03c5e4087fc84deb195f39c0f
Ali Firas <ali.albdaer.py@gmail.com>
2024-08-26 17:15:19 +0300

    Option

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -17,7 +17,9 @@ impl PlayerState {
     }
 }
 
+#[allow(unused)]  // suppress warnings
 fn main() {
+    // Enums
     let mut player1 = PlayerState::Alive { hp: 20 };
     dbg!(&player1);
 
@@ -25,7 +27,27 @@ fn main() {
     dbg!(&player1);
 
     match player1 {
-        PlayerState::Alive { hp } => println!("Alive, hp: {}", player1.hp()),
+        PlayerState::Alive { hp: _ } => println!("Alive, hp: {}", player1.hp()),  // hp: _ means we don't care about the value
         PlayerState::Dead => println!("Dead, hp: {}", player1.hp()),
     }
+
+    // Option
+    let five: i32 = 5;
+    let some_5 = Some(5);
+    let no_num: Option<i32> = None;
+
+    // let sum = five + no_num;  // bad (no implementation for `i32 + Option<i32>`)
+    // use Option<T> with match.
+    /*
+    methods:
+    - is_some() -> bool
+    - is_none() -> bool
+    - unwrap() -> T (panics if the value is None) 
+    - unwrap_or(T) -> T (returns the value or a default)
+    - expect(&str) -> T (panics with a custom message)
+    - as_ref() -> Option<&T> (converts from &Option<T> to Option<&T>)
+    - as_mut() -> Option<&mut T> (converts from &mut Option<T> to Option<&mut T>)
+    - Others: https://doc.rust-lang.org/std/option/enum.Option.html
+     */
+
 }
